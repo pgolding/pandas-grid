0733d485694aeda08d2f3cd6ed5137090932b6c2
pgolding <goldingp@gmail.com>
2017-03-05 14:49:55 -0800

    Improved comments in grid.py

--- griddy/grid.py
+++ griddy/grid.py
@@ -5,13 +5,17 @@ import os
 import webbrowser
 import pandas as pd
 
+# Use pkg_resources to get the correct path name to the locally packaged resources
 from pkg_resources import resource_filename
 grid_html = resource_filename('griddy', 'resources/grid.html')
 grid_css = resource_filename('griddy', 'resources/grid.css')
 
+# Array of column CSS classes indexed by number of columns
+# e.g. If table has 4 columns, then return COLS[3] - i.e. 'col-3' as the default col width
 COLS = ['col-12', 'col-6', 'col-4', 'col-3', 'col-2', 'col-2', 'col-1', \
         'col-1', 'col-1', 'col-1', 'col-1', 'col-1']
 
+# Helper function to determine default column CSS class
 def get_column_styling(num_cols_in_table):
     if num_cols_in_table <= 12:
         return COLS[num_cols_in_table-1]
@@ -27,7 +31,7 @@ def render_table(table_data: pd.DataFrame, headings: str =[], filename='griddy.h
     # if columns css not given, then calculate a default for all cols
     if not css:
         col_style = get_column_styling(table_data.shape[1])
-        css = [col_style] * 3
+        css = [col_style] * num_of_cols # Apply same column CSS class to all columns
     # Parse image cols and calculate their index to pass into template renderer
     img_cols=[]
     if headings:
@@ -44,9 +48,10 @@ def render_table(table_data: pd.DataFrame, headings: str =[], filename='griddy.h
         rendered = template.render(title=filename.split('.')[0], col_css=css, \
                     img_cols = img_cols, table=table_data.as_matrix(), \
                     headings=headings, doc_header=annotation)
+        #Save rendered HTML to current working directory
         with open(filename, 'w') as rendered_file:
             rendered_file.writelines(rendered)
-        #Put the CSS file in the local directory if it doesn't exist already
+        #And finally put the CSS file in the local directory if it doesn't exist already
         if not os.path.isfile ('grid.css'):
             os.system ("cp {} {}".format(grid_css, 'grid.css'))
         path = os.path.realpath(filename)
@@ -54,10 +59,6 @@ def render_table(table_data: pd.DataFrame, headings: str =[], filename='griddy.h
         if show:
             webbrowser.open(file_url, new=1)
         return file_url
-
     except Exception as e:
         print(e)
-
-
-
-#render_table(table_data, [3])
+        return None
